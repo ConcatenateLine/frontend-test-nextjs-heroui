755cde329b4ebf13382ddc75c213d71a8ef13a64
Concatenate Line <concatenateline@gmail.com>
2025-07-24 00:27:41 -0600

    feat: Configure Docker for development and production

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,12 @@ import { button as buttonStyles } from "@heroui/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
+import SectionTitle from "@/components/sectionTitle";
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <SectionTitle title="Next.js 13" />
       <div className="inline-block max-w-xl text-center justify-center">
         <span className={title()}>Make&nbsp;</span>
         <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
